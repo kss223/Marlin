1120ebbb4b735b33fe233544121a9148aabbc784
unknown <shik@corp.uber.com>
2020-07-27 11:22:56 -0400

    my own mods

--- buildroot/share/scripts/findMissingTranslations.sh
+++ buildroot/share/scripts/findMissingTranslations.sh
@@ -9,9 +9,7 @@
 # If no language codes are specified then all languages will be checked
 #
 
-LANGHOME="Marlin/src/lcd/language"
-
-[ -d $LANGHOME ] && cd $LANGHOME
+[ -d "Marlin" ] && cd "Marlin"
 
 FILES=$(ls language_*.h | grep -v -E "(_en|_test)\.h" | sed -E 's/language_([^\.]+)\.h/\1/')
 declare -A STRING_MAP
@@ -29,8 +27,8 @@ fi
 
 echo -n "Building list of missing strings..."
 
-for i in $(awk '/Language_Str/{print $3}' language_en.h); do
-  [[ $i == "MSG_CUBED" ]] && continue
+for i in $(awk '/#ifndef/{print $2}' language_en.h); do
+  [[ $i == "LANGUAGE_EN_H" ]] && continue
   LANG_LIST=""
   for j in $TEST_LANGS; do
     [[ $(grep -c " ${i} " language_${j}.h) -eq 0 ]] && LANG_LIST="$LANG_LIST $j"
@@ -42,8 +40,5 @@ done
 echo
 
 for K in $( printf "%s\n" "${!STRING_MAP[@]}" | sort ); do
-  case "$#" in
-    1 ) echo $K ;;
-    * ) printf "%-35s :%s\n" "$K" "${STRING_MAP[$K]}" ;;
-  esac
+  printf "%-35s :%s\n" "$K" "${STRING_MAP[$K]}"
 done
