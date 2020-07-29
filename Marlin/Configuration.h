35439512f02c05784f1855879edca91800b9816b
Kowin Shi <shik@uber.com>
2020-07-29 19:57:45 -0400

    added newest changes to config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -313,9 +313,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  #define  DEFAULT_Kp 20.27
+  #define  DEFAULT_Ki 3.75
+  #define  DEFAULT_Kd 27.40
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
